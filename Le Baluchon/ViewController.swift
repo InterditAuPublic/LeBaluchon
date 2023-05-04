8682df245b432087c770336c2587668126d9a3b4
Melvin Poutrel <melvin.poutrel@itinnove.com>
2023-05-04 16:34:24 +0200

    Initial Commit

---
+++ Le Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Le Baluchon
+//
+//  Created by Melvin Poutrel on 04/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
