0ebfbab4d01baa0abea1c86362d4043c6f7bd603
Melvin Poutrel <melvin.poutrel@itinnove.com>
2023-05-12 02:44:06 +0200

    WIP Weather

--- Le Baluchon/Controller/WeatherViewController.swift
+++ Le Baluchon/Controller/WeatherViewController.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import UIKit
+
+class WeatherViewController: UIViewController {
+    private let model = WeatherModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        prepareView()
+    }
+
+    func prepareView() {
+        titleLabel.text = "Weather"
+        // cityTextField.placeholder = "City"
+        // getWeatherButton.setTitle("Get Weather", for: .normal)
+        // getWeatherButton.setTitleColor(.white, for: .normal)
+        // getWeatherButton.backgroundColor = .systemBlue
+        // getWeatherButton.layer.cornerRadius = 5
+        // getWeatherButton.layer.masksToBounds = true
+        // activityIndicator.isHidden = true
+    }
+
+    @IBOutlet weak var titleLabel: UILabel!
+   
+   
+    
+}
--- Le Baluchon/Model/Weather.swift
+++ Le Baluchon/Model/Weather.swift
@@ -6,3 +6,77 @@
 //
 
 import Foundation
+
+// MARK: - Weather
+
+class WeatherModel {
+    
+    // MARK: - Properties
+    var accessKey: String
+    var baseURL: String
+    var json = """
+    {
+      "coord": {
+        "lon": 10.99,
+        "lat": 44.34
+      },
+      "weather": [
+        {
+          "id": 501,
+          "main": "Rain",
+          "description": "moderate rain",
+          "icon": "10d"
+        }
+      ],
+      "base": "stations",
+      "main": {
+        "temp": 298.48,
+        "feels_like": 298.74,
+        "temp_min": 297.56,
+        "temp_max": 300.05,
+        "pressure": 1015,
+        "humidity": 64,
+        "sea_level": 1015,
+        "grnd_level": 933
+      },
+      "visibility": 10000,
+      "wind": {
+        "speed": 0.62,
+        "deg": 349,
+        "gust": 1.18
+      },
+      "rain": {
+        "1h": 3.16
+      },
+      "clouds": {
+        "all": 100
+      },
+      "dt": 1661870592,
+      "sys": {
+        "type": 2,
+        "id": 2075663,
+        "country": "IT",
+        "sunrise": 1661834187,
+        "sunset": 1661882248
+      },
+      "timezone": 7200,
+      "id": 3163858,
+      "name": "Zocca",
+      "cod": 200
+    }
+"""
+                            
+    
+    // MARK: - Initializer
+    init() {
+        guard let path = Bundle.main.path(forResource: "Keys", ofType: "plist"),
+              let keys = NSDictionary(contentsOfFile: path),
+              let accessKey = keys["OpenWeatherApiKey"] as? String,
+              let baseURL = keys["OpenWeatherBaseURL"] as? String else {
+            fatalError("Unable to read keys from Keys.plist")
+        }
+        self.accessKey = accessKey
+        self.baseURL = baseURL
+    }
+    
+}
