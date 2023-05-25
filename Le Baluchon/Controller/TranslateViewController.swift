8dc192ea0543bcda9a6291fcba9b65abcde1066f
Melvin Poutrel <melvin.poutrel@itinnove.com>
2023-05-25 12:03:21 +0200

    Add Translation feature

--- Le Baluchon/Controller/TranslateViewController.swift
+++ Le Baluchon/Controller/TranslateViewController.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+import UIKit
+
+// MARK: - TranslateViewController
+
+class TranslateViewController: UIViewController {
+        
+        // MARK: - Properties
+        private let translationService = TranslationServiceImplementation()
+        private var textToTranslate: String = ""
+        private var translatedText: String = ""
+        
+        // MARK: View Life cycles
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            prepareView()
+        }
+        
+        // MARK: Prepare View
+        private func prepareView() {
+            textToTranslateTextField.placeholder = "Text to translate"
+            translatedTextLabel.text = "TRANSLATED TEXT"
+            translateButton.setTitle("Translate", for: .normal)
+            translateButton.setTitleColor(.white, for: .normal)
+            translateButton.backgroundColor = .systemBlue
+            translateButton.layer.cornerRadius = 5
+            translateButton.layer.masksToBounds = true
+            activityIndicator.isHidden = true
+        }
+        
+        // MARK: - Outlets
+        @IBOutlet weak var textToTranslateTextField: UITextField!
+        @IBOutlet weak var translatedTextLabel: UILabel!
+        @IBOutlet weak var translateButton: UIButton!
+        @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+        
+        // MARK: - Actions
+        @IBAction func onTranslateTapped(_ sender: UIButton) {
+            activityIndicator.isHidden = false
+            activityIndicator.startAnimating()
+            textToTranslateTextField.resignFirstResponder()
+            textToTranslate = textToTranslateTextField.text ?? ""
+            textToTranslate = textToTranslate.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
+            translationService.getTranslation(request: TranslationRequest(source: "fr", target: "en", text: textToTranslate)) { (success, response) in
+                self.activityIndicator.isHidden = true
+                self.activityIndicator.stopAnimating()
+                if success, let response = response {
+                    print("response", response)
+                    self.translatedText = response.translatedText
+                    self.translatedTextLabel.text = self.translatedText
+                } else {
+                    self.translatedTextLabel.text = "Error"
+                }
+            }
+        }
+}
--- Le Baluchon/Model/Translation.swift
+++ Le Baluchon/Model/Translation.swift
@@ -6,3 +6,103 @@
 //
 
 import Foundation
+
+// MARK: - Translation
+struct Translation: Codable {
+    let data: DataClass
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let translations: [TranslationElement]
+}
+
+// MARK: - TranslationElement
+struct TranslationElement: Codable {
+    let translatedText: String
+}
+
+// MARK: - TranslationError
+struct TranslationError: Codable {
+    let error: Error
+}
+
+// MARK: - Error
+struct Error: Codable {
+    let code: Int
+    let message: String
+}
+
+// MARK: - TranslationRequest
+
+struct TranslationRequest {
+    let source: String
+    let target: String
+    let text: String
+}
+
+// MARK: - TranslationResponse
+struct TranslationResponse {
+    let translatedText: String
+}
+
+// MARK: - TranslationService
+
+protocol TranslationService {
+    func getTranslation(request: TranslationRequest, callback: @escaping (Bool, TranslationResponse?) -> Void)
+}
+
+class TranslationServiceImplementation: TranslationService {
+
+    var task: URLSessionDataTask?
+    var session = URLSession(configuration: .default)
+    var translation: Translation?
+    var accessKey = String()
+    var baseURL = String()
+
+    init(session: URLSession = URLSession(configuration: .default)) {
+        self.session = session
+        guard let path = Bundle.main.path(forResource: "Keys", ofType: "plist"),
+              let keys = NSDictionary(contentsOfFile: path),
+              let accessKey = keys["CloudTranslationApiKey"] as? String,
+              let baseURL = keys["CloudTranslationBaseURL"] as? String else {
+            fatalError("Unable to read keys from Keys.plist")
+        }
+        self.accessKey = accessKey
+        self.baseURL = baseURL
+    }
+
+    func getTranslation(request: TranslationRequest, callback: @escaping (Bool, TranslationResponse?) -> Void) {
+//       print("getTranslation")
+        print("\(baseURL)?key=\(accessKey)&source=\(request.source)&target=\(request.target)&q=\(request.text)")
+        guard let url = URL(string: "\(baseURL)?key=\(accessKey)&source=\(request.source)&target=\(request.target)&q=\(request.text)") else {
+            callback(false, nil)
+            return
+        }
+       task?.cancel()
+        task = session.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                print("data: \(String(describing: data))")
+                print("response: \(String(describing: response))")
+                print("error: \(String(describing: error))")
+
+                guard let data = data, error == nil else {
+                    callback(false, nil)
+                    return
+                }
+                guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                    callback(false, nil)
+                    return
+                }
+                guard let responseJSON = try? JSONDecoder().decode(Translation.self, from: data) else {
+                    callback(false, nil)
+                    return
+                }
+                let translationResponse = TranslationResponse(translatedText: responseJSON.data.translations[0].translatedText)
+                callback(true, translationResponse)
+                print(translationResponse)
+            }
+        }
+        task?.resume()
+    }
+}
